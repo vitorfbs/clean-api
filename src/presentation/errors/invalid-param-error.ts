b07b3980377e81f75e9d4f1c1452efde2bc8e3e6
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 18:55:33 -0300

    fix: add name to the constructor to prevent issues with inheritance in of parameter errors

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,5 +1,6 @@
 export class InvalidParamError extends Error {
     constructor(paramName: string) {
         super(`The ${paramName} provided is invalid`)
+        this.name = 'InvalidParamError'
     }
 }
