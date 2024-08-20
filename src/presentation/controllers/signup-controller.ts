f50395fe376f88b3c964b8b2f15df63c5c8900e7
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 16:24:50 -0300

    feat: add validation for body in SignUpController

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,9 +1,18 @@
 export class SignUpController {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     handle (httpRequest: any): any {
-        return {
-            statusCode: 400,
-            body: new Error("Missing param: name")
+        if (!httpRequest.body.name){
+            return {
+                statusCode: 400,
+                body: new Error("Missing param: name")
+            }
         }
+        if (!httpRequest.body.email){
+            return {
+                statusCode: 400,
+                body: new Error("Missing param: email")
+            }
+        }
+        return;
+        
     }
 }
