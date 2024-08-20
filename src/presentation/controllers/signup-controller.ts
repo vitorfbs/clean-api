0e0a37f4398024f83e514162ba84a93970be7eb5
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 11:56:10 -0300

    feat: add SignUp Controller

---
+++ src/presentation/controllers/signup-controller.ts
@@ -0,0 +1,8 @@
+export class SignUpController {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    handle (httpRequest: any): any {
+        return {
+            statusCode: 400
+        }
+    }
+}
