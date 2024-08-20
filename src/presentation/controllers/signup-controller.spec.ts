c5c09321adb5c6ddccf86f5d885888a8826fafbc
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 16:24:27 -0300

    fix: remove unused import

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,5 +1,4 @@
-import exp from "constants";
-import { SignUpController } from "./signup-controller";
+import { SignUpController } from "./signup-controller"
 
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
@@ -15,5 +14,20 @@ describe('SignUp Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error("Missing param: name"))       
-    });
-});
+    })
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error("Missing param: email"))       
+    })
+})
