a17e3de320fdcdad1d80747083b028fc8d08faca
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 18:40:51 -0300

    test: ensure SignUpController's EmailValidator is called with the correct email

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -93,4 +93,19 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)   
         expect(httpResponse.body).toEqual(new InvalidParamError('email'))     
     })
+
+    test('Should call EmailValidator with correct email', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+        const HttpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        sut.handle(HttpRequest)
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+    })
 })
