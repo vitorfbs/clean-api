b2210894d1cdb2bea20ae9984ee891036ad8d7a3
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 16:13:23 -0300

    feat: change return for handle() method of Signup Controller to return a body

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -2,7 +2,8 @@ export class SignUpController {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     handle (httpRequest: any): any {
         return {
-            statusCode: 400
+            statusCode: 400,
+            body: new Error("Missing param: name")
         }
     }
 }
