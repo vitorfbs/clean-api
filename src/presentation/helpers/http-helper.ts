7226a9ec87f4be67605ebade2b87c2402b568866
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 19:20:35 -0300

    feat: add serverError

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,7 +1,13 @@
-import { BAD_REQUEST } from "http-status";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "http-status";
 import { HttpResponse } from "../protocols/http";
+import { ServerError } from "../errors/server-error";
 
 export const badRequest = (error: Error): HttpResponse => ({
     statusCode: BAD_REQUEST,
     body: error
+})
+
+export const serverError = (): HttpResponse => ({
+    statusCode: INTERNAL_SERVER_ERROR,
+    body: new ServerError
 })
