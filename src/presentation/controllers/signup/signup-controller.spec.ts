c7bb456125002a289a15c574ef4fa9578c6f19ad
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-21 10:04:30 -0300

    chore: test new husky config

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -56,7 +56,7 @@ describe('SignUp Controller',  () => {
         }
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissingParamError("email"))       
+        expect(httpResponse.body).toEqual(new MissingParamError("bugabuga"))       
     })
 
     test('Should return 400 if no email is provided', async () => {
