a29bb2ae74a65845798cc7a525b5e9303e3931d7
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-21 14:58:59 -0300

    feat: add AddAccountRepository to DbAddAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,18 +1,22 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './db-add-account-protocols';
+import { hash } from 'crypto';
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Encrypter } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter
+    private readonly addAccountRepository: AddAccountRepository
 
-    constructor (encrypter: Encrypter) {
+    constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
         this.encrypter = encrypter
+        this.addAccountRepository = addAccountRepository
     }
-    async add (account: AddAccountModel): Promise<AccountModel> {
-        const hashed_password = await this.encrypter.encrypt(account.password)
-        return new Promise(resolve => resolve({
-            id: '',
-            name: '',
-            email: '',
-            password: ''
-        }))
+    async add (accountData: AddAccountModel): Promise<AccountModel> {
+        const hashed_password = await this.encrypter.encrypt(accountData.password)
+        const account = await this.addAccountRepository.execute({
+            name: accountData.name,
+            email: accountData.email,
+            password: hashed_password
+        })
+
+        return new Promise(resolve => resolve(account))
     }
 }
