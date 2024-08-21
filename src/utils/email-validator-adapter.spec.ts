45fd9214a24a8ebf2cb347c9ff79746114129f32
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-21 10:31:50 -0300

    test: mock validator return for EmailValidatorAdapter tests

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,13 +1,35 @@
 import { EmailValidatorAdapter } from "./email-validator-adapter"
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail (): boolean {
+        return true
+    }
+}))
+
+interface SutTypes {
+    sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutTypes => {
+    const sut = new EmailValidatorAdapter()
+    return {
+        sut
+    }
+}
 
 describe('EmailValidatorAdapter', () => {
   test('should return false when validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const { sut } = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
-  test('should ', () => {
-    
+
+  test('should return true when validator returns true', () => {
+    const { sut } = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
   test('should ', () => {
     
