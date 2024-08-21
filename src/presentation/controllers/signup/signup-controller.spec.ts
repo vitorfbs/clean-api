f003529f7fb969a5a737bf5f9ff2c0e76846cdbd
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-21 10:10:27 -0300

    test: add another commit test for husky

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -56,7 +56,7 @@ describe('SignUp Controller',  () => {
         }
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissingParamError("thisshouldnotworkagain"))       
+        expect(httpResponse.body).toEqual(new MissingParamError("invalid"))       
     })
 
     test('Should return 400 if no email is provided', async () => {
