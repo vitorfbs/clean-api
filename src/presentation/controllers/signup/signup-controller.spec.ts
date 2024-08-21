fe59688775e4a30ff4c1fef99ba2ca5915c521d7
Bem Silva <vitorfbs.ti@gmail.com>
2024-08-20 22:53:33 -0300

    test: ensure SignUpController returns 200 when data is valid

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -215,4 +215,24 @@ describe('SignUp Controller', () => {
         expect(httpResponse.body).toEqual(new ServerError())
 
     })
+
+    test('Should return 200 when valid data is provided', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(201)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@email.com',
+            password: 'valid_password'
+        })
+    })
 })
